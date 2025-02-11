4da4304b36d9fde1b2fb034d55c037aa09ec1dc3
ChrisAlister2347218 <chris.alister@mca.christuniversity.in>
2025-02-11 22:46:11 +0530

    Fix ASGI configuration and deployment settings

--- chat_project/asgi.py
+++ chat_project/asgi.py
@@ -1,15 +1,16 @@
 import os
-import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from chat.routing import websocket_urlpatterns
 
-# Set Django settings module
+# Set Django settings module before importing anything that uses Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_project.settings')
 
-# Initialize Django ASGI application early
+# Import after setting DJANGO_SETTINGS_MODULE
+import django
 django.setup()
+
+from chat.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
