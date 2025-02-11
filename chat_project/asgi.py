dd545c7ff3733ac4de4aceb24e228e83a4811b16
ChrisAlister2347218 <chris.alister@mca.christuniversity.in>
2025-02-11 22:41:09 +0530

    Update deployment configuration to use gunicorn with uvicorn workers

--- chat_project/asgi.py
+++ chat_project/asgi.py
@@ -1,17 +1,15 @@
-# chat_project/asgi.py
 import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_project.settings')
-
+import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
-from django.conf import settings
-import django
+from chat.routing import websocket_urlpatterns
+
+# Set Django settings module
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_project.settings')
 
 # Initialize Django ASGI application early
 django.setup()
-
-from chat.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
@@ -19,3 +17,6 @@ application = ProtocolTypeRouter({
         URLRouter(websocket_urlpatterns)
     ),
 })
+
+# Bind to the PORT environment variable (used by Render)
+port = os.environ.get("PORT", "10000")  # Default to 10000 if PORT is not set
